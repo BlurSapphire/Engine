5e411f71f7168bd8493882dc88a433fba0acfcd3
BlurSapphire <relampagopop@gmail.com>
2025-03-15 23:28:39 +0000

    Update mesh.h

--- backend/mesh.h
+++ backend/mesh.h
@@ -4,10 +4,19 @@
 #include<vector>
 #include<GL/glew.h>
 
-using std::vector;
+using std::vector, glm::vec3;
 
 
 class Mesh {
 public:
-	Mesh();
+	Mesh(vector<vec3> vertices, vector<GLuint> indices);
+	~Mesh();
+
+	void Draw(GLuint shaderProgram);
+
+private:
+	GLuint VAO, VBO, EBO;
+	GLuint shaderProgram;
+	vector<vec3> vertices;
+	vector<GLuint> indices;
 };
