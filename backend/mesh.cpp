58f9fdb4b3b54281a639204e023474f439dcbb83
BlurSapphire <relampagopop@gmail.com>
2025-03-16 22:29:36 +0000

    .

--- backend/mesh.cpp
+++ backend/mesh.cpp
@@ -10,5 +10,28 @@ Mesh::Mesh(vector<vec3> vertices, vector<GLuint> indices) {
 
 	glBindVertexArray(VAO);
 
-	 
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size()* sizeof(vec3), &vertices[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &vertices[0], GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+}
+
+Mesh::~Mesh() {
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+}
+
+void Mesh::Draw(GLuint shaderProgram) {
+	glUseProgram(shaderProgram);
+
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, indices.size(),GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
 }
